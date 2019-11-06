089481ffcfacdf724fc8ac666517d7b0b6621aaa
Anchali Lee <emilylee927@gmail.com>
2019-11-05 20:21:43 -0600

    render Shop

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -9,9 +9,25 @@ class Shop extends Component {
     }
 
     render() {
+        const items = this.props.allProducts.map(
+            ({ product_id, name, description, price, image_url }) => {
+                return (
+                    <div key={product_id}>
+                        <h3>{name}</h3>
+                        <p>{description}</p>
+                        <p>Price: ${price}</p>
+                        <img src={image_url} alt={name}></img>
+                        <br></br>
+                        <button>Add to cart</button>
+                    </div>
+                );
+            }
+        );
+
         return (
             <div>
                 <h2>Shop</h2>
+                {items}
             </div>
         );
     }
